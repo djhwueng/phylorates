d7c1870070f78bba0a3df8cb60ec45262ef34bf3
Tony Jhwueng <tony.jhwueng@gmail.com>
2022-07-06 15:50:00 +0800

    test mcmc for posterior

---
+++ testmcmc.R
@@ -0,0 +1,84 @@
+rm(list=ls())
+source("~/Dropbox/Conference/1Prof/202207ECAISBA/rcode/modelphylorate.R")
+###################################################
+#            main program                         #
+###################################################
+ntaxa<-5
+tree<-rtree(ntaxa)
+phen<-fastBM(tree,internal=T)
+trait<-phen[1:ntaxa]
+
+params<-priorsample(prmodel="halfnorm",prtausq="invgamma")
+
+LikelihoodAll(params=params,trait=trait,tree=tree,modeltype="garch00")
+LikelihoodAll(params=params,trait=trait,tree=tree,modeltype="garch10")
+LikelihoodAll(params=params,trait=trait,tree=tree,modeltype="garch11")
+
+posteriorAll(params,trait=trait,tree=tree,prmodel="halfnorm",prtau="invgamma",modeltype="garch00")
+posteriorAll(params,trait=trait,tree=tree,prmodel="halfnorm",prtau="invgamma",modeltype="garch10")
+posteriorAll(params,trait=trait,tree=tree,prmodel="halfnorm",prtau="invgamma",modeltype="garch11")
+
+#reselect startvalues
+startvalue<-c(0.1,0.1,1)
+names(startvalue)<-c("kappa","alpha","tausq")
+posteriorAll(startvalue,trait=trait,tree=tree,prmodel="halfnorm",prtau="invgamma",modeltype="garch11")
+
+# next can do mcmc shall be work better now
+#https://khayatrayen.github.io/MCMC.html
+
+iter<-1e4
+chain<-array(dim=c(iter+1, length(startvalue)))
+colnames(chain)<-names(params)
+chain[1,]<-startvalue
+head(chain)
+for (i in 1:iter){
+#  i<-1
+  print(i)
+  #Tony you shall use a symmetric proposal, normal distribution shall be nice that avoud negative. This is equal to 1 if the proposal density is symmetric. 
+  proposal = proposalfunction(chain[i,])
+  print(round(proposal,4))
+  new<-posteriorAll(proposal,trait=trait,tree=tree,prmodel="halfnorm",prtau="invgamma",modeltype="garch00")
+  old<-posteriorAll(chain[i,],trait=trait,tree=tree,prmodel="halfnorm",prtau="invgamma",modeltype="garch00")
+  probab= exp(  new  -  old   )
+  if(runif(1)<probab){
+    chain[i+1,]<-proposal
+  }else{
+    chain[i+1,]<-chain[i,]
+  }
+}
+
+burnIn<-5000
+par(mfrow=c(1,3))
+plot(chain[-(1:burnIn),"kappa"],main="kappa",type="l")
+plot(chain[-(1:burnIn),"alpha"],main="alpha",type="l")
+plot(chain[-(1:burnIn),"tausq"],main="tausq",type="l")
+
+acceptance=1-mean(duplicated(chain[-(1:burnIn),]))
+acceptance
+
+par(mfrow = c(2,3))
+hist(chain[-(1:burnIn),"kappa"],nclass=30, , main="Posterior of kappa", xlab="True value = red line" )
+abline(v = median(chain[-(1:burnIn),"kappa"]),col="blue",lwd=3)
+abline(v = 0, col="red",lty=2 )
+
+hist(chain[-(1:burnIn),"alpha"],nclass=30, main="Posterior of alpha", xlab="True value = red line")
+abline(v = median(chain[-(1:burnIn),"alpha"]),col="blue",lwd=3)
+abline(v = 0, col="red",lty=2 )
+
+hist(chain[-(1:burnIn),"tausq"],nclass=30, main="Posterior of tausq", xlab="")
+abline(v = median(chain[-(1:burnIn),"tausq"]) ,col="blue",lwd=2)
+#abline(v = 0.5, col="red" )
+
+plot(chain[-(1:burnIn),"kappa"], type = "l", xlab="True value = red line" , main = "Chain values of kappa", )
+abline(h = 0, col="red" )
+
+plot(chain[-(1:burnIn),"alpha"], type = "l", xlab="True value = red line" , main = "Chain values of alpha", )
+abline(h = 0, col="red" )
+
+plot(chain[-(1:burnIn),"tausq"], type = "l", xlab="True value = red line" , main = "Chain values of tausq", )
+#abline(h = 0.5, col="red" )
+
+
+
+
+
