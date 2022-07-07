1e73fe1f288f7b46264aba8b0472ddd988f4f33b
Tony Jhwueng <tony.jhwueng@gmail.com>
2022-07-07 17:43:49 +0800

    update use true parameters to sim trait

--- testmcmc.R
+++ testmcmc.R
@@ -3,13 +3,54 @@ source("~/Dropbox/Conference/1Prof/202207ECAISBA/rcode/modelphylorate.R")
 ###################################################
 #            main program                         #
 ###################################################
+# ntaxa<-5
+# tree<-rtree(ntaxa)
+# phen<-fastBM(tree,internal=T)
+# trait<-phen[1:ntaxa]
+# trait
+# dim(trait)
+# typeof(trait)
+# tree$tip.label
+###################################################
+#            sim trait  using true parameter      #
+###################################################
+
 ntaxa<-5
+kappa<-0.002
+alpha<-0.006
+tausq<-1
+betas<-rnorm(2*ntaxa-1)
 tree<-rtree(ntaxa)
-phen<-fastBM(tree,internal=T)
-trait<-phen[1:ntaxa]
+tree1<-tree
+makeL(tree)->L
+makeL1(tree)->L1
+names(betas)<-colnames(L)
+trait<-array(NA,c(2*ntaxa-1)) #this is Garch(1,1)
+names(trait)<-colnames(L)
+betas<-betas[c(ntaxa:(2*ntaxa-1),1:(ntaxa-1))]
+trait<-trait[c(ntaxa:(2*ntaxa-1),1:(ntaxa-1))]
+trait[names(trait)==ntaxa+1] <- betas[ntaxa+1]
+betas
+trait
+tree<-reorder(tree,"postorder")
+tree$root.edge<-0
+N<-dim(tree$edge)[1]
+anc<-tree$edge[,1]
+des<-tree$edge[,2]
+treelength<-tree$edge.length
+
+for(index in N:1){
+  trait[des[index]]<- betas[des[index]]+ kappa/treelength[index]*betas[anc[index]]*rnorm(1,0,treelength[index])^2  + alpha/treelength[index]*betas[anc[index]] + rnorm(1,0,tausq)
+}  
+trait<-c(trait[1:ntaxa])
+dim(trait)
+trait
+tree<-tree1#no postorder
+tree$tip.label
+plot(tree)
+####
 
 params<-priorsample(prmodel="halfnorm",prtausq="invgamma")
-
 LikelihoodAll(params=params,trait=trait,tree=tree,modeltype="garch00")
 LikelihoodAll(params=params,trait=trait,tree=tree,modeltype="garch10")
 LikelihoodAll(params=params,trait=trait,tree=tree,modeltype="garch11")
@@ -26,17 +67,18 @@ posteriorAll(startvalue,trait=trait,tree=tree,prmodel="halfnorm",prtau="invgamma
 # next can do mcmc shall be work better now
 #https://khayatrayen.github.io/MCMC.html
 
-iter<-1e4
+iter<-1e5
 chain<-array(dim=c(iter+1, length(startvalue)))
 colnames(chain)<-names(params)
 chain[1,]<-startvalue
 head(chain)
 for (i in 1:iter){
+  if(i%%1e3==0){print(i)}
 #  i<-1
-  print(i)
+  #print(i)
   #Tony you shall use a symmetric proposal, normal distribution shall be nice that avoud negative. This is equal to 1 if the proposal density is symmetric. 
   proposal = proposalfunction(chain[i,])
-  print(round(proposal,4))
+  #print(round(proposal,4))
   new<-posteriorAll(proposal,trait=trait,tree=tree,prmodel="halfnorm",prtau="invgamma",modeltype="garch00")
   old<-posteriorAll(chain[i,],trait=trait,tree=tree,prmodel="halfnorm",prtau="invgamma",modeltype="garch00")
   probab= exp(  new  -  old   )
@@ -47,7 +89,7 @@ for (i in 1:iter){
   }
 }
 
-burnIn<-5000
+burnIn<-50000
 par(mfrow=c(1,3))
 plot(chain[-(1:burnIn),"kappa"],main="kappa",type="l")
 plot(chain[-(1:burnIn),"alpha"],main="alpha",type="l")
